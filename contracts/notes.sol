9e8df51cb97b44ba2905d45866650d13130c71a4
dudesahn <dudesahn@gmail.com>
2021-04-20 19:18:36 -0400

    Update notes.sol

--- contracts/notes.sol
+++ contracts/notes.sol
@@ -26,3 +26,18 @@ to claim your rewards, you call claimBalance(startIndex, endIndex) on the pool c
 - Does IL protection work the same if you deposit BNT? 30-day, 100-day, etc?
 - what's the best way to view my start index and end index?
 - Can yearn stake/vote with our vBNT? Does Bancor feel one way or another about this?
+
+Asked in their dev chat:
+Looking into the contracts, I have a few questions for interacting with the BNT contracts:
+
+- Does claiming rewards via claimBalance reset your multiplier? If so, is there a way to view on-chain what your current multiplier is?
+- Does the IL protection function the same way (30-day min, 100-day max) if you deposit BNT single-sided? Or does BNT get special consideration?
+- What’s the best way to view the start index and end index for a position? I’m a little unclear about how this works, but it seems that it’s needed when calling claimBalance. 
+- It’s my understanding that if a user deposits multiple times, these are all managed separately. How can all of these positions be viewed/tracked on the smart contract level?
+
+- response from their CTO:
+1. claimBalance isn't for claiming rewards but rather for claiming BNT withdrew from pools, so it doesn't reset the multiplier
+2. you can query the multiplier per provider/pool reserve through the staking rewards contract (registry.addressOf('StakingRewards'))
+3. IL protection works the same way for both TKN and BNT
+4. start/end indices are just 0 based indices for locked balances - you can query the count and then pass in (0, count - 1) or you can just call claimBalance(0, 100) - it's very unlikely that you have more than a few locked balances
+5. the LiquidityProtectionStore contract holds the metadata for these positions, so you can call protectedLiquidityIds(provider) to get the list and then query each one using protectedLiquidity(id)
